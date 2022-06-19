0e9fa649936cb2103df8e049a8d538fcff3ff295
robertarthury <robertarthury@gmail.com>
2022-06-19 13:58:40 -0400

    fixing a bug or 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,20 @@
-const APP_PREFIX = `BudgetTracker-`
-const VERSION = 'version_01';
-const DATA_CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = `budget-tracker-v1`
+const DATA_CACHE_NAME = 'data-cach-v1';
 
 const FILES_TO_CACHE = [
-  '/index.html',
-  '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/js/index.js',
-  '/assets/js/ibd.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/icons/icon-152x152.png',
-  '/assets/images/icons/icon-192x192.png',
-  '/assets/images/icons/icon-384x384.png',
-  '/assets/images/icons/icon-512x512.png'
+  './index.html',
+  './manifest.json',
+  './assets/css/styles.css',
+  './assets/js/index.js',
+  './assets/js/idb.js',
+  './assets/icons/icon-72x72.png',
+  './assets/icons/icon-96x96.png',
+  './assets/icons/icon-128x128.png',
+  './assets/icons/icon-144x144.png',
+  './assets/icons/icon-152x152.png',
+  './assets/icons/icon-192x192.png',
+  './assets/icons/icon-384x384.png',
+  './assets/icons/icon-512x512.png'
 ];
 
 // Install the service worker
@@ -47,18 +46,41 @@ self.addEventListener('install', function(evt) {
 });
 
 // Intercept fetch requests
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url);
-  e.respondWith(
-      caches.match(e.request).then(function (request) {
-          if (request) { // if cache is available, respond with cache
-              console.log('responding with cache : ' + e.request.url);
-              return request
-          } else {       // if there are no cache, try fetching request
-              console.log('file is not cached, fetching : ' + e.request.url);
-              return fetch(e.request)
-          }
+self.addEventListener('fetch', function(evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-      })
-  )
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function() {
+      return caches.match(evt.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get('accept').includes('text/html')) {
+          // return the cached home page for all requests for html pages
+          return caches.match('/');
+        }
+      });
+    })
+  );
 });
